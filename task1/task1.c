93db32b85da7768837a04ae1d814e4b532460819
kcarri14 <kcarri14@calpoly.edu>
2024-10-30 09:33:33 -0700

    imconfused

---
+++ task1/task1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "task1.h"
+
+
+
+int str_lower(char *stdinupt, int buffer){
+
+}
+
+int main(int argc, char* argv[]){
+    char buffer[1024];
+    char standard_input[1024] = gets(stdin);
+    str_lower(*standard_input, buffer);
+
+}
