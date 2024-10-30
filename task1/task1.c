5726dc102a9d673622dd0c731d6aec7941ce2b6e
kcarri14 <kcarri14@calpoly.edu>
2024-10-30 09:53:17 -0700

    Task1 done

--- task1/task1.c
+++ task1/task1.c
@@ -4,7 +4,21 @@
 
 
 
-int str_lower(char *stdinupt, int buffer){
+void str_lower(char *stdinput, char *buffer){
+    int i = 0;
+    while(stdinput[i] != '\0'){
+        buffer[i] = tolower(stdinput[i]);
+        i++;
+    }
+    buffer[i] = "\0";
+
+}
+void str_lower_mutate(char *str){
+    int i = 0;
+    while(str[i] != '\0'){
+        str[i] = tolower(str[i]);
+        i++;
+    }
 
 }
 
@@ -12,5 +26,7 @@ int main(int argc, char* argv[]){
     char buffer[1024];
     char standard_input[1024] = gets(stdin);
     str_lower(*standard_input, buffer);
+    printf("%s\n", buffer);
 
+    return 0;
 }
--- task1/task1_tests.c
+++ task1/task1_tests.c
@@ -11,7 +11,25 @@ void test1() {
    checkit_string(result, expected);
 }
 
+void test2() {
+   char input[] = "KAItLyN nAMvaR";
+   char result[20];
+   char *expected = "kaitlyn namvar";
+
+   str_lower(input, result);
+   checkit_string(result, expected);
+}
+void test3() {
+   char input[] = "kAVeH nAMvaR";
+   char *expected = "kaveh namvar";
+
+   str_lower_mutate(input);
+   checkit_string(input, expected);
+}
+
 int main(void) {
    test1();
+   test2();
+   test3();
    return 0;
 }
