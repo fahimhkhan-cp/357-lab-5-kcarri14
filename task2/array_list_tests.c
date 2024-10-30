f6466b883dd6f40764a46935b1940f3e80ae4650
kcarri14 <kcarri14@calpoly.edu>
2024-10-30 10:46:44 -0700

    task2 again

--- task2/array_list_tests.c
+++ task2/array_list_tests.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "array_list.h"
 
-int main(){
+void test_resize(){
     array_list *list = array_list_new();
     array_list_add_to_end(list, "Hello");
     array_list_add_to_end(list, "My");
@@ -11,12 +11,21 @@ int main(){
     array_list_add_to_end(list, "Kaitlyn");
     array_list_add_to_end(list, "Namvar");
 
+    checkit_string(list->items[0], "Hello");
+    checkit_string(list->items[1], "My");
+    checkit_string(list->items[2], "Name");
+    checkit_string(list->items[3], "will");
+    checkit_string(list->items[4], "be");
+    checkit_string(list->items[5], "Kaitlyn");
+    checkit_string(list->items[0], "Namvar");
+
+    checkit_int(list->length, 3);
+
     for (int i =0; i < list -> length; i++){
         printf("Item %d: %s\n", i, list->items[i]);
     }
-
     array_list_free(list);
-    return 0;
+
 }
 
 
